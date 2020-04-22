7f5e0709a1d9cd20e20c5fa41dedf6c4e19c9c57
CHIHO <chiho001@naver.com>
2020-04-22 18:28:51 +0900

    카메라 캘리브레이션 코드 추가

--- irst_video.py
+++ irst_video.py
@@ -1,18 +1,18 @@
-#video 출력 소스
-import cv2
-capture = cv2.VideoCapture("Saved_Video_lookdown.raw")
-
-while True:
-    if capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT):
-        capture.open("Saved_Video_lookdown.raw")
-
-    ret, frame = capture.read()
-    cv2.imshow("VideoFrame", frame)
-
-    if cv2.waitKey(33) > 0: break
-
-capture.release() #VideoCapture의 장치를 닫고 메모리를 해제
-cv2.destroyAllWindows()
+# #video 출력 소스
+# import cv2
+# capture = cv2.VideoCapture("Saved_Video_lookdown.raw")
+#
+# while True:
+#     if capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT):
+#         capture.open("Saved_Video_lookdown.raw")
+#
+#     ret, frame = capture.read()
+#     cv2.imshow("VideoFrame", frame)
+#
+#     if cv2.waitKey(33) > 0: break
+#
+# capture.release() #VideoCapture의 장치를 닫고 메모리를 해제
+# cv2.destroyAllWindows()
 
 # 구문오류 -> syntax error
 # 런타임에러
