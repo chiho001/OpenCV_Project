c469a801a5ec695d28fe220c619670f36154ae1e
CHIHO <chiho001@naver.com>
2020-04-27 20:14:41 +0900

    1. 캡쳐 및 녹화 상이나 이미지를 캡쳐하거나 녹화하기 위해 사용합니다. 영상이나 이미지를 연속적 또는 순간적으로 캡쳐하거나 녹화할 수 있습니다. 2. 트랙 바 사용 3. 채널 분리(Split) 및 병합(Merge) 4. 그래픽 드로잉(Drawing) 5. 기하학적 변환(Warp Perspective)

--- irst_video.py
+++ irst_video.py
@@ -1,15 +1,15 @@
 #video 출력 소스
-# capture = cv2.VideoCapture("Star.mp4")
-#
-# while True:
-#     if capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT):
-#         capture.open("Star.mp4")
-#
-#     ret, frame = capture.read()
-#     cv2.imshow("VideoFrame", frame)
-#
-#     if cv2.waitKey(33) > 0: break
-#
-# capture.release() #VideoCapture의 장치를 닫고 메모리를 해제
-# cv2.destroyAllWindows()
-# a = [input().split(" ")]
+capture = cv2.VideoCapture("Star.mp4")
+
+while True:
+    if capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT):
+        capture.open("Star.mp4")
+
+    ret, frame = capture.read()
+    cv2.imshow("VideoFrame", frame)
+
+    if cv2.waitKey(33) > 0: break
+
+capture.release() #VideoCapture의 장치를 닫고 메모리를 해제
+cv2.destroyAllWindows()
+
--- opencv_practice.py
+++ opencv_practice.py
@@ -1,3 +1,204 @@
+
+
+
+
+##############################################################
+#트랙바
+#트랙 바는 일정 범위 내의 값을 변경할 때 사용하며, 적절한 임곗값을 찾거나 변경하기 위해 사용합니다.
+#OpenCV의 트랙 바는 생성된 윈도우 창에 트랙바를 부착해 사용할 수 있습니다.
+# import cv2
+#
+# def onChange(pos):
+#     pass
+#
+# src = cv2.imread("ferrari.jpg", cv2.IMREAD_GRAYSCALE)
+#
+# cv2.namedWindow("Trackbar Windows")
+#
+# cv2.createTrackbar("threshold", "Trackbar Windows", 0, 255, onChange)
+# cv2.createTrackbar("maxValue", "Trackbar Windows", 0, 255, lambda x : x)
+#
+# cv2.setTrackbarPos("threshold", "Trackbar Windows", 127)
+# cv2.setTrackbarPos("maxValue", "Trackbar Windows", 255)
+#
+# while cv2.waitKey(1) != ord('q'):
+#
+#     thresh = cv2.getTrackbarPos("threshold", "Trackbar Windows")
+#     maxval = cv2.getTrackbarPos("maxValue", "Trackbar Windows")
+#
+#     _, binary = cv2.threshold(src, thresh, maxval, cv2.THRESH_BINARY)
+#
+#     cv2.imshow("Trackbar Windows", binary)
+#
+# cv2.destroyAllWindows()
+##############################################################
+#캡처 및 녹화
+#영상이나 이미지를 캡쳐하거나 녹화하기 위해 사용합니다. 영상이나 이미지를 연속적 또는 순간적으로 캡쳐하거나 녹화할 수 있습니다.
+import datetime
+import cv2
+
+capture = cv2.VideoCapture("Star.mp4")
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+record = False
+
+while True:
+    if (capture.get(cv2.CAP_PROP_POS_FRAMES) == capture.get(cv2.CAP_PROP_FRAME_COUNT)):
+        capture.open("Star.mp4")
+
+    ret, frame = capture.read()
+    cv2.imshow("VideoFrame", frame)
+
+    now = datetime.datetime.now().strftime("%d_%H-%M-%S")
+    key = cv2.waitKey(33)
+
+    if key == 27:
+        break
+    elif key == 26:
+        print("캡쳐")
+        cv2.imwrite("D:/" + str(now) + ".png", frame)
+    elif key == 24:
+        print("녹화 시작")
+        record = True
+        video = cv2.VideoWriter("D:/" + str(now) + ".avi", fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+    elif key == 3:
+        print("녹화 중지")
+        record = False
+        video.release()
+
+    if record == True:
+        print("녹화 중..")
+        video.write(frame)
+
+capture.release()
+cv2.destroyAllWindows()
+
+##############################################################
+# 기하학적 변환(Warp Perspective)
+# 영상이나 이미지 위에 기하학적으로 변환하기 위해 사용. 영상이나 이미지 펼치거나 좁힐 수 있다.
+# Tip : WarpPerspective의 경우 4개의 점을 매핑합니다. (4개의 점을 이용한 변환)
+# Tip : WarpAffine의 경우 3개의 점을 매핑합니다. (3개의 점을 이용한 변환)
+# import numpy as np
+# import cv2
+#
+# src = cv2.imread("ferrari.jpg", cv2.IMREAD_COLOR)
+# height, width, channel = src.shape
+# srcPoint = np.array([[300, 200], [400, 200], [500, 500], [200, 500]], dtype=np.float32)
+# dstPoint = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+# # 원본 이미지에서 4점 변환할 srcPoint와 결과 이미지의 위치가 될 dstPoint를 선언합니다.
+# # 좌표의 순서는 좌상, 우상, 우하, 좌하 순서입니다. numpy 형태로 선언하며, 좌표의 순서는 원본 순서와 결과 순서가 동일해야합니다.
+# # Tip : dtype을 float32 형식으로 선언해야 사용할 수 있습니다.
+# matrix = cv2.getPerspectiveTransform(srcPoint, dstPoint)
+# # 원근법 제거
+# # 기하학적 변환을 위하여 cv2.getPerspectiveTransform(원본 좌표 순서, 결과 좌표 순서)를 사용하여 matrix를 생성합니다.
+# # 다음과 같은 형식으로 매트릭스가 생성됩니다.
+# dst = cv2.warpPerspective(src, matrix, (width, height))
+# # 위 함수를 거쳐야만 변환행렬값을 적용하여 최종 결과 이미지 얻을 수 있다.
+# # cv2.warpPerspective(원본 이미지, 매트릭스, (결과 이미지 너비, 결과 이미지 높이))를 사용하여 이미지를 변환할 수 있습니다.
+# # 저장된 매트릭스 값을 사용하여 이미지를 변환합니다.
+# # 이외에도, 보간법, 픽셀 외삽법을 추가적인 파라미터로 사용할 수 있습니다.
+# cv2.imshow("dst", dst)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+##############################################################
+#그래픽 드로잉(Drawing)
+
+#영상이나 이미지 위에 그래픽을 그리기 위해 사용합니다. 선, 원, 사각형 등을 그릴 수 있습니다.
+
+# shift : 좌표를 Shift(비트 연산)만큼 이동시켜 표시합니다.
+# offset : 좌표를 (x, y)만큼 이동시켜 표시합니다.#
+#
+# import numpy as np
+# import cv2
+#
+# src = np.zeros((768, 1366, 3), dtype = np.uint8)
+#
+# cv2.line(src, (100, 100), (1200, 100), (255, 0, 0), 3, cv2.LINE_AA)
+# #cv2.line(이미지, (x1, y1), (x2, y2), (B, G, R), 두께, 선형 타입)을 이용하여 선을 그릴 수 있습니다.
+# # (x1, y1)과 (x2, y2)가 연결된 (B, G, R) 색상, 두께 굵기의 선을 그릴 수 있습니다.
+# # 선형 타입은 선의 연결성을 의미합니다.
+#
+# cv2.circle(src, (300, 300), 50, (0, 255, 0), cv2.FILLED, cv2.LINE_4)
+# # cv2.circle(이미지, (x, y), 반지름, (B, G, R), 두께, 선형 타입)을 이용하여 원을 그릴 수 있습니다.
+# # (x, y) 중심점을 가지는 반지름 크기로 설정된 (B, G, R) 색상, 두께 굵기의 원을 그릴 수 있습니다.
+# # Tip : 내부를 채우는 경우, 두께를 cv2.FILLED을 사용하여 내부를 채울 수 있습니다.
+#
+# cv2.rectangle(src, (500, 200), (1000, 400), (255, 0, 0), 5, cv2.LINE_8)
+# # cv2.rectangle(이미지, (x1, y1), (x2, y2), (B, G, R), 두께, 선형 타입)을 이용하여 사각형을 그릴 수 있습니다.
+# # (x1, y1)의 좌측 상단 모서리와 (x2, y2)의 우측 하단 모서리가 연결된 (B, G, R) 색상, 두께 굵기의 사각형을 그릴 수 있습니다.
+#
+# cv2.ellipse(src, (1200, 300), (100, 50), 0, 90, 180, (255, 255, 0), 2)
+# # cv2.ellipse(이미지, (x, y), (lr, sr), 각도, 시작 각도, 종료 각도, (B, G, R), 두께, 선형 타입)을 이용하여 타원을 그릴 수 있습니다.
+# # (x, y)의 중심점을 가지며 중심에서 가장 먼 거리를 가지는 lr과 가장 가까운 거리를 가지는 sr의 타원을 각도만큼 기울어진 타원를 생성합니다.
+# # 시작 각도와 종료 각도를 설정하여 호의 형태로 그리며 (B, G, R) 색상, 두께 굵기의 타원을 그릴 수 있습니다.
+# # Tip : 선형 타입은 설정하지 않아도 사용할 수 있습니다.
+#
+# pts1 = np.array([[100,50],[300,50],[200,600]])
+#
+# pts2 = np.array([[600, 500], [800, 500], [700, 600]])
+#
+# #poly 함수를 사용하는 경우, numpy 형태로 저장된 위치 좌표들이 필요합니다.
+# #n개의 점이 저장된 경우, n각형을 그릴 수 있습니다.
+#
+#
+# cv2.polylines(src, [pts1], True, (0, 255, 255), 2)
+# # cv2.polylines(이미지, [위치 좌표], 닫힘 유/무, (B, G, R), 두께, 선형 타입 )을 이용하여 다각형을 그릴 수 있습니다.
+# # [위치 좌표]들의 지점들을 가지며 시작점과 도착점이 연결되어있는지 닫힘 유/무를 설정하여 (B, G, R) 색상, 두께 굵기의 다각형을 그릴 수 있습니다.
+# #cv2.fillPoly(src, [pts2], (255, 0, 255), cv2.LINE_AA)
+# # cv2.fillPoly(이미지, [위치 좌표], (B, G, R), 두께, 선형 타입 )을 이용하여 내부가 채워진 다각형을 그릴 수 있습니다.
+# # [위치 좌표]들의 지점들을 가지며 (B, G, R) 색상, 두께 굵기의 내부가 채워진 다각형을 그릴 수 있습니다.
+# #cv2.putText(src, "CHIHO", (900,600), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 3)
+# # cv2.putText(이미지, 문자, (x, y), 글꼴, 글자 크기, (B, G, R), 두께, 선형 타입)을 이용하여 문자를 그릴 수 있습니다.
+# # 문자 내용을 가지는 문자열을 (x, y) 위치에 표시합니다. 글꼴와 글자 크기를 가지며 (B, G, R) 색상, 두께 굵기의 문자를 그릴 수 있습니다.
+# # Tip : 문자의 위치는 좌표의 좌측 하단을 기준으로 글자가 생성됩니다.
+#
+# cv2.imshow("Result", src)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+##############################################################
+# 채널 분리(Split) 및 병합(Merge)
+# 영상이나 이미지를 채널을 나누고 합치기 위해 사용합니다. 채널을 B(Blue), G(Green), R(Red)로 분리하여 채널을 변환할 수 있습니다.
+# 빈 공간(흑백 이미지)을 이용하여 한 채널 아예 지워버리기. Red 지워버리기 ㅋㅋ
+# Tip : OpenCV의 가산혼합의 삼원색 기본 배열순서는 BGR입니다..
+# b, g, r = cv2.split(이미지)를 이용하여 채널을 분리합니다.
+# 채널에 순서의 맞게 각 변수에 대입됩니다.
+# 분리된 채널들은 단일 채널이므로 흑백의 색상으로만 표현됩니다.
+# cv2.merge((채널1, 채널2, 채널3))을 이용하여 나눠진 채널을 다시 병합할 수 있습니다.
+# 채널을 변형한 뒤에 다시 합치거나 순서를 변경하여 병합할 수 있습니다.
+# 순서가 변경될 경우, 원본 이미지와 다른 색상으로 표현됩니다.
+# Additional Information
+# numpy 형식 채널 분리
+# b = src[:,:,0]
+# g = src[:,:,1]
+# r = src[:,:,2]
+# 이미지[높이, 너비, 채널]을 이용하여 특정 영역의 특정 채널만 불러올 수 있습니다.
+#
+# :, :, n을 입력할 경우, 이미지 높이와 너비를 그대로 반환하고 n번째 채널만 반환하여 적용합니다.
+# 빈 이미지
+# height, width, channel = src.shape
+# zero = np.zeros((height, width, 1), dtype = np.uint8)
+# bgz = cv2.merge((b, g, zero))
+# 검은색 빈 공간 이미지가 필요할 때는 np.zeros((높이, 너비, 채널), dtype=정밀도)을 이용하여 빈 이미지를 생성할 수 있습니다.
+# Blue, Green, Zero이미지를 병합할 경우, Red 채널 영역이 모두 흑백이미지로 변경됩니다.
+# Tip : import numpy as np가 포함된 상태여야합니다.>
+# import cv2
+# import numpy as np
+#
+# src = cv2.imread("ferrari.jpg",cv2.IMREAD_COLOR)
+# height, width, channel = src.shape
+# b, g, r = cv2.split(src)
+# inversebgr = cv2.merge((r,g,b))
+# zero = np.zeros((height,width,1),dtype=np.uint8)
+# bgz = cv2.merge((b, g, zero))
+#
+# cv2.imshow("b",b)
+# cv2.imshow("g",g)
+# cv2.imshow("r",r)
+# cv2.imshow("inverse", inversebgr)
+# cv2.imshow("bgz", bgz)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
 ##############################################################
 #채널 범위 병합(addWeighted)
 #영상이나 이미지를 색상을 검출 할 때, cv2.inRange()의 영역이 한정되어 색상을 설정하는 부분이 한정되어 있습니다.
@@ -15,22 +216,22 @@
 # 두 이미지의 채널을 그대로 합칠 예정이므로 각각의 비율은 1.0으로 사용하고, 가중치는 사용하지 않으므로 0.0을 할당합니다.
 #
 # cv2.inRange()를 사용할 때, 단일 채널 이미지의 범위만 할당하여 병합할 수 도 있습니다.
-import cv2
-
-src = cv2.imread("ferrari.jpg", cv2.IMREAD_COLOR)
-hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-h, s, v = cv2.split(hsv)
-
-lower_red = cv2.inRange(hsv, (0, 100, 100), (5, 255, 255))
-upper_red = cv2.inRange(hsv, (170, 100, 100), (180, 255, 255))
-added_red = cv2.addWeighted(lower_red, 1.0, upper_red, 1.0, 0.0)
-
-red = cv2.bitwise_and(hsv, hsv, mask = added_red)
-red = cv2.cvtColor(red, cv2.COLOR_HSV2BGR)
-
-cv2.imshow("red", red)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# import cv2
+#
+# src = cv2.imread("ferrari.jpg", cv2.IMREAD_COLOR)
+# hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+# h, s, v = cv2.split(hsv)
+#
+# lower_red = cv2.inRange(hsv, (0, 100, 100), (5, 255, 255))
+# upper_red = cv2.inRange(hsv, (170, 100, 100), (180, 255, 255))
+# added_red = cv2.addWeighted(lower_red, 1.0, upper_red, 1.0, 0.0)
+#
+# red = cv2.bitwise_and(hsv, hsv, mask = added_red)
+# red = cv2.cvtColor(red, cv2.COLOR_HSV2BGR)
+#
+# cv2.imshow("red", red)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 ##############################################################
 # HSV(Hue, Saturation, Value)
